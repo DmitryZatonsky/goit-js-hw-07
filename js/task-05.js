ee8bede29873e79268a146047e55f39d2abbd41f
Dmitry Zatonsky <83658538+DmitryZatonsky@users.noreply.github.com>
2021-10-19 18:12:44 +0300

    таска 5 менше строк

--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,8 @@
 let inPut = document.querySelector('#name-input');
 const outPut = document.querySelector('#name-output');
 
-inPut.oninput = removingSpaces;
-
-function removingSpaces () {
-  inPut.value = inPut.value.trim();
-};
-
 inPut.addEventListener('input', (event) => {
-  inPut.value.length === 0
+  inPut.value.trim() === ''
     ? outPut.textContent = 'незнакомец'
     : outPut.textContent = `${event.target.value}`
 });
